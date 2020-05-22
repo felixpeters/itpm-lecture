2e3144f848573732dd228e25d471ee7d9c759b7e
Felix Peters <felix@albrechtpeters.de>
2020-05-22 08:26:25 +0200

    update tests to account for grade steps

--- modules/grade_calculator.test.js
+++ modules/grade_calculator.test.js
@@ -4,6 +4,6 @@ test('grade average of 1 and 1 is 1', () => {
     expect(gradeCalculator.calc(1, 1)).toBe(1);
 });
 
-test('grade average of 1 and 1.3 is 1.2', () => {
-    expect(gradeCalculator.calc(1, 1.3)).toBe(1.2);
+test('grade average of 1 and 1.3 is 1.3', () => {
+    expect(gradeCalculator.calc(1, 1.3)).toBe(1.3);
 });
