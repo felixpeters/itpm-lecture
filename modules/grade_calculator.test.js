59e2315fcdbbb4b45b3e6e173b644e116a800fdb
Felix Peters <felix@albrechtpeters.de>
2020-05-29 12:55:48 +0200

    add further test case for grade calculator

--- modules/grade_calculator.test.js
+++ modules/grade_calculator.test.js
@@ -7,3 +7,7 @@ test('grade average of 1 and 1 is 1', () => {
 test('grade average of 1 and 1.3 is 1.3', () => {
     expect(gradeCalculator.calc(1, 1.3)).toBe(1.3);
 });
+
+test('grade average 2.3 and 1.3 is 1.7', () => {
+    expect(gradeCalculator.calc(2.3, 1.3)).toBe(1.7);
+});
